a97f481efe324d624d54c8d6a532d4154a653c79
Rubens Mariuzzo <rubens@mariuzzo.com>
2020-11-01 00:05:16 -0400

    First version.

---
+++ src/__tests__/easy-peasy-thunk-stages.test.ts
@@ -0,0 +1,83 @@
+import { createStore, thunk, Thunk, action } from 'easy-peasy'
+
+import { thunkStagesModel, ThunkStagesModel } from '../easy-peasy-thunk-stages'
+
+describe('easy-peasy-thunk-stages', () => {
+  it('should create store', () => {
+    createStore(thunkStagesModel({}))
+  })
+
+  it('should set initial thunk stages values', () => {
+    type TestModel = {
+      test: Thunk<TestModel>
+    }
+    const testModel: TestModel & ThunkStagesModel<TestModel> = {
+      test: thunk(jest.fn()),
+      ...thunkStagesModel({
+        test: 'idle'
+      })
+    }
+    const store = createStore(testModel)
+    expect(store.getState().thunkStages).toHaveProperty('test', 'idle')
+  })
+
+  it('should set "busy" and "completed" stage of a thunk', () => {
+    type TestModel = {
+      test: Thunk<TestModel>
+    }
+    const setThunkStageSpy = jest.fn()
+    const testModel: TestModel & ThunkStagesModel<TestModel> = {
+      test: thunk(jest.fn()),
+      ...thunkStagesModel({
+        test: 'idle'
+      }),
+      setThunkStage: action((state, payload) => {
+        setThunkStageSpy(payload)
+      })
+    }
+    const store = createStore(testModel)
+    store.getActions().test()
+    expect(setThunkStageSpy).toHaveBeenCalledWith({
+      thunk: 'test',
+      stage: 'busy'
+    })
+    expect(setThunkStageSpy).toHaveBeenCalledWith({
+      thunk: 'test',
+      stage: 'completed'
+    })
+  })
+
+  it('should set "busy" and "failed" stage of a thunk', async () => {
+    jest.useFakeTimers()
+    type TestModel = {
+      test: Thunk<TestModel>
+    }
+    const setThunkStageSpy = jest.fn()
+    const testModel: TestModel & ThunkStagesModel<TestModel> = {
+      test: thunk(() => Promise.reject()),
+      ...thunkStagesModel({
+        test: 'idle'
+      }),
+      setThunkStage: action((state, payload) => {
+        setThunkStageSpy(payload)
+      })
+    }
+    const store = createStore(testModel)
+    try {
+      await store.getActions().test()
+      fail()
+    } catch {
+      // Intentionally left blank.
+    }
+    setImmediate(() => {
+      expect(setThunkStageSpy).toHaveBeenCalledWith({
+        thunk: 'test',
+        stage: 'busy'
+      })
+      expect(setThunkStageSpy).toHaveBeenCalledWith({
+        thunk: 'test',
+        stage: 'failed'
+      })
+    })
+  })
+})
---
+++ src/easy-peasy-thunk-stages.ts
@@ -0,0 +1,100 @@
+import {
+  action,
+  Action,
+  Generic,
+  generic,
+  Thunk,
+  ThunkCreator,
+  thunkOn,
+  ThunkOn
+} from 'easy-peasy'
+
+type ThunkStage = 'idle' | 'busy' | 'completed' | 'failed'
+
+type ThunkNames<M extends Record<string, unknown>> = {
+  [K in keyof M]: M[K] extends Thunk<any, any, any, any, any> ? K : never
+}[keyof M]
+
+export interface ThunkStagesModel<M extends Record<string, unknown>> {
+  thunkStages: Generic<
+    Record<ThunkNames<Omit<M, keyof ThunkStagesModel<any>>>, ThunkStage>
+  >
+  setThunkStage: Action<
+    ThunkStagesModel<M>,
+    {
+      thunk: ThunkNames<Omit<M, keyof ThunkStagesModel<any>>>
+      stage: ThunkStage
+    }
+  >
+  setThunkStageOn: ThunkOn<ThunkStagesModel<M>>
+}
+
+export const thunkStagesModel = <M extends Record<string, unknown>>(
+  initialThunkStages: Record<
+    ThunkNames<Omit<M, keyof ThunkStagesModel<any>>>,
+    ThunkStage
+  >
+): ThunkStagesModel<M> => {
+  return {
+    thunkStages: generic(initialThunkStages),
+    setThunkStage: action((state, payload) => {
+      state.thunkStages[payload.thunk] = payload.stage
+    }),
+    setThunkStageOn: thunkOn(
+      (actions) => {
+        return (Object.keys(actions) as Array<keyof typeof actions>)
+          .filter(
+            (actionName) =>
+              'type' in actions[actionName] &&
+              'startType' in actions[actionName] &&
+              'successType' in actions[actionName] &&
+              'failType' in actions[actionName]
+          )
+          .reduce<Array<string>>((p, actionName) => {
+            const action = <ThunkCreator | undefined>(
+              Object.values(actions).find(
+                (action) => action.type.split('.').pop() === actionName
+              )
+            )
+            if (action) {
+              p.push(
+                action.type,
+                action.startType,
+                action.successType,
+                action.failType
+              )
+            }
+            return p
+          }, [])
+      },
+      async (actions, target) => {
+        const action = Object.values(actions).find(
+          (action) => action.type === target.type.split('(')[0]
+        )
+        if (!action) {
+          throw new Error(`Could not find action by type: ${target.type}`)
+        }
+        const thunkName = action.type.split('.').pop()!
+        if (
+          !(
+            'startType' in action &&
+            'successType' in action &&
+            'failType' in action
+          )
+        ) {
+          throw new Error(`The action type: ${target.type} is not a thunk`)
+        }
+        const thunkAction = <ThunkCreator>action
+        const newStage: ThunkStage =
+          target.type === thunkAction.startType
+            ? 'busy'
+            : target.type === thunkAction.successType
+            ? 'completed'
+            : target.type === thunkAction.failType
+            ? 'failed'
+            : 'idle'
+        actions.setThunkStage({ thunk: thunkName as any, stage: newStage })
+      }
+    )
+  }
+}
